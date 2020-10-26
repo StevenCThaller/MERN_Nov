10edd0d7f5ae647b87e27f2e8f29f75dbab8c718
Cody Thaller <stevencthaller@gmail.com>
2020-10-26 08:30:47 -0700

    First commit - Bubble and Selection sort

---
+++ Algos/bubble_sort.js
@@ -0,0 +1,16 @@
+/* 
+  https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+  Stable sort
+  
+  Time Complexity
+    - Best:     O(n) linear when array is already sorted
+    - Average:  O(n^2) quadratic
+    - Worst:    O(n^2) quadratic when the array is reverse sorted
+  Space: O(1) constant
+  For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+  For every pair of adjacent indicies, swap them if the item on the left is larger than the item on the right until array is fully sorted
+*/
+
+function bubbleSort(array){
+    
+}
