e0d257ba9ffbfc3c66ffc328e3f4add84383a271
Cody Thaller <stevencthaller@gmail.com>
2020-10-28 08:47:27 -0700

    Merge Sort

---
+++ Algos/merge_sort.js
@@ -0,0 +1,25 @@
+/* 
+  http://algorithms.dojo.news/static/Algorithms/index.html#LinkTarget_2140
+  Stable sort
+  Visualization: https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+  Time Complexity
+    - Best case:    O(n log(n)) linearithmic
+    - Average case: O(n log(n)) linearithmic
+    - Worst case:   O(n log(n)) linearithmic
+  Space: O(n) linear
+*/
+
+//  steps:
+//    1. create a merge function to merge two already sorted arrays into a single sorted array
+//      - you do NOT need to work in place, ok to use a new array
+ function merge(arr1, arr2){
+     
+}
+
+//    2. create mergeSort function to sort the provided array
+//      - split the array in half and recursively merge the halves using the previously created merge function
+//      - splitting of arrays stops when array can no longer be split
+//      - an array of 1 item is by definition sorted, so two arrays of 1 item can therefore be merged into a sorted array
+function mergeSort(array){
+
+}
