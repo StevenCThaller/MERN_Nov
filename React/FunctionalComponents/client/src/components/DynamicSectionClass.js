28c7e2f2695982aba62605632804ed00409ae071
Cody Thaller <stevencthaller@gmail.com>
2020-10-30 14:40:56 -0700

    useReducer

--- React/FunctionalComponents/client/src/components/DynamicSectionClass.js
+++ React/FunctionalComponents/client/src/components/DynamicSectionClass.js
@@ -1,48 +1,48 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-// class DynamicSection extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             haveRead: false,
-//             name: "Cody"
-//         }
-//     }
+class DynamicSection extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            haveRead: false,
+            name: "Cody"
+        }
+    }
 
-//     readToggle = () => {
-//         this.setState({ haveRead: true })
-//     }
+    readToggle = () => {
+        this.setState({ haveRead: true })
+    }
 
-//     render() {
-//         const { header, text } = this.props;
-//         const { haveRead, name } = this.state;
-//         return (
-//             <>
-//                 <h1>{header}</h1>
-//                 <h2>{name}</h2>
-//                 <p>{text}  
-//                 {
-//                     haveRead ?
-//                     <span>You already read this</span>
-//                     :
-//                     <button className="btn btn-primary" onClick={ this.readToggle }>Read</button>
-//                 }
-//                 </p>
-//             </>
-//         )
-//     }
-// }
+    render() {
+        const { header, text } = this.props;
+        const { haveRead, name } = this.state;
+        return (
+            <>
+                <h1>{header}</h1>
+                <h2>{name}</h2>
+                <p>{text}  
+                {
+                    haveRead ?
+                    <span>You already read this</span>
+                    :
+                    <button className="btn btn-primary" onClick={ this.readToggle }>Read</button>
+                }
+                </p>
+            </>
+        )
+    }
+}
 
-// export default DynamicSection
+export default DynamicSection
 
 
-// // Ternary Operation
+// Ternary Operation
 
-// // 3 main parts to the ternary operator
-//     // 1. The condition that is determining the outcome (this is what would normally go into the parentheses of an if check)
-//     // 2. Action to take if the condition is true
-//     // 3. Action to take if the condition is false
-// // condition ? action1 : action2
+// 3 main parts to the ternary operator
+    // 1. The condition that is determining the outcome (this is what would normally go into the parentheses of an if check)
+    // 2. Action to take if the condition is true
+    // 3. Action to take if the condition is false
+// condition ? action1 : action2
 
 
 
--- React/FunctionalComponents/client/src/components/FormComponent.js
+++ React/FunctionalComponents/client/src/components/FormComponent.js
@@ -1,47 +1,84 @@
+// import React, { useReducer } from 'react'
 import React, { useState } from 'react'
 
+// Initialization for the useReducer
+// const initialState = {
+//     size: "",
+//     weight: 0,
+//     type: "",
+//     color: ""
+// }
+
+// function reducer(state, action){
+//     return {
+//         ...state,
+//         [action.type]: action.payload
+//     }
+// }
+
+
 const FormComponent = props => {
-    const [size, setSize] = useState("");
-    const [weight, setWeight] = useState(0);
-    const [type, setType] = useState("");
-    const [color, setColor] = useState("");
+    // Incorporating the useReducer with state
+    // const [pumpkin, setPumpkin] = useReducer(reducer, initialState);
 
-    // const [pumpkin, setPumpkin] = useState({
-    //     size: "",
-    //     weight: "",
-    //     type: "",
-    //     color: ""
-    // })
+    // const [size, setSize] = useState("");
+    // const [weight, setWeight] = useState(0);
+    // const [type, setType] = useState("");
+    // const [color, setColor] = useState("");
 
+
+    // Cody's preferred process: step 1 - initialize state
+    const [pumpkin, setPumpkin] = useState({
+        size: "",
+        weight: "",
+        type: "",
+        color: ""
+    });
+
+
+    // Cody's preferred process: step 3 - build your change handler
+    const changeHandler = e => {
+        // For useReducer
+        // setPumpkin({
+        //     type: e.target.name,
+        //     payload: e.target.value
+        // });
+        setPumpkin({
+            ...pumpkin,
+            [e.target.name]: e.target.value
+        })
+    }
     
 
 
     const submitHandler = e => {
         e.preventDefault();
-        console.log(`Size: ${size}`);
-        console.log(`Weight: ${weight}`);
-        console.log(`Type: ${type}`);
-        console.log(`Color: ${color}`);
+        console.log(`Size: ${pumpkin.size}`);
+        console.log(`Weight: ${pumpkin.weight}`);
+        console.log(`Type: ${pumpkin.type}`);
+        console.log(`Color: ${pumpkin.color}`);
     }
 
     return (
         <div>
+            {/* Cody's preferred process: step 2 - make the form */}
             <form onSubmit={ submitHandler }>
                 <div className="form-group row">
                     <label htmlFor="size" className="col-sm-4">Size: </label>
-                    <input type="text" name="size" className="col-sm-8 form-control" onChange={ e => setSize(e.target.value) } value={size}/>
+                                                                    {/* Cody's preferred process: step 4 - attach the handler to the inputs */}
+                    <input type="text" name="size" className="col-sm-8 form-control" onChange={ changeHandler }/>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="weight" className="col-sm-4">Weight: </label>
-                    <input type="number" name="weight" className="col-sm-8 form-control" onChange={ e => setWeight(e.target.value) } />
+                    <input type="number" name="weight" className="col-sm-8 form-control" onChange={ changeHandler } />
                 </div>
                 <div className="form-group row">
                     <label htmlFor="type" className="col-sm-4">Type: </label>
-                    <input type="text" name="type" className="col-sm-8 form-control" onChange={ e => setType(e.target.value) }/>
+                    <input type="text" name="type" className="col-sm-8 form-control" onChange={ changeHandler }/>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="color" className="col-sm-4">Color: </label>
-                    <input type="text" name="color" className="col-sm-8 form-control" onChange={ e => setColor(e.target.value) }/>
+                    <input type="text" name="color" className="col-sm-8 form-control" onChange={ changeHandler }/>
                 </div>
                 <div className="form-group row">
                     <input type="submit" value="Submit Pumpkin!" className="col-sm-4 offset-sm-4 btn btn-primary"/>
