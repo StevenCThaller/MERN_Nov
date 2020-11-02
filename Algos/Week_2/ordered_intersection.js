95aab7cf89cfa35862f6fde47c67f3d8e4ec02f2
Cody Thaller <stevencthaller@gmail.com>
2020-11-02 11:20:05 -0800

    Lifting State and algo solution

--- Algos/Week_2/ordered_intersection.js
+++ Algos/Week_2/ordered_intersection.js
@@ -6,5 +6,24 @@
 */
 
 function orderedIntersection(array1, array2){
-    
+    let i1 = 0; // for iterating through array1
+    let i2 = 0; // for iterating through array2
+
+    const ordered = []; // to store our ordered intersection into
+
+    while(i1 < array1.length && i2 < array2.length){ // until we've gone through everything in both arrays
+        if(array1[i1] === array2[i2]){ // check to see if the values are the same
+            if(ordered[ordered.length-1] !== array1[i1]){ // and if the value hasn't already been added
+                ordered.push(array1[i1]); // add it to the output array
+            }
+            i1++; // iterate both
+            i2++; // iterators
+        } else if(array1[i1] < array2[i2]){ // if we find that the element we're looking at in array 1 is smaller than the element
+            i1++; // we're looking at in array 2, increment i1 by 1
+        } else {
+            i2++; // otherwise increment i2 by 1
+        }
+    }
+
+    return ordered;
 }
