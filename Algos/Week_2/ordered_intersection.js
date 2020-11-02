a78cccdf3ba47a0df0043938431b71b23f235ee2
Cody Thaller <stevencthaller@gmail.com>
2020-11-02 08:33:48 -0800

    Ordered Intersection algo prompt

---
+++ Algos/Week_2/ordered_intersection.js
@@ -0,0 +1,10 @@
+/* 
+    Efficiently combine two already sorted multiset arrays 
+    into an array containing the sorted set intersection of the two.
+    Output: only the shared values between the two arrays (deduped).
+    Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+function orderedIntersection(array1, array2){
+    
+}
