080379cbad77d52df57de844166de70cb16bd879
Cody Thaller <stevencthaller@gmail.com>
2020-11-05 08:40:11 -0800

    Non consecutive numbers

---
+++ Algos/Week_2/all_nonconsecutive.js
@@ -0,0 +1,13 @@
+/* 
+    Given an array of ints, find all the non-consecutive integers
+    A number is non-consecutive if it is NOT exactly 1 larger than the previous element.
+    The first element is never considered non-consecutive.
+    Return an array of objects where each object contains the number itself
+    and it's index.
+*/
+function nonConsecutive(array){
+
+}
+
+
+nonConsecutive([1,3,4,5,7,8,9,11,14]);
