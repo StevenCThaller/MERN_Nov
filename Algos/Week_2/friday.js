9d8e3a606a6ed8d2fe1802ae96ef7fa20e68de9e
Cody Thaller <stevencthaller@gmail.com>
2020-11-06 08:49:46 -0800

    Prep for 11/6

---
+++ Algos/Week_2/friday.js
@@ -0,0 +1,18 @@
+/*
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    Bonus: Make it O(n) time
+*/
+function twoSum(array, target) {
+
+}
+
+/* 
+    Given an unsorted non-empty array of integers and int k, return the k most frequent elements (in any order)
+    You can assume there is always a valid solution
+    These example inputs are sorted for readability, but the input is NOT guaranteed to be sorted and the output does NOT need to be in any specific order
+    Hard Bonus: make it O(n) time
+*/
+function kMostFrequent(array, k){
+    
+}
---
+++ Mongo/MongoIntro/server/models/models.js
@@ -0,0 +1,18 @@
+class Fighter {
+    constructor(){
+        this.name = "";
+        this.design = "",
+        this.abilities = [];
+    }
+}
+
+class OtherClass { 
+    constructor(){
+        this.field1 = "wat";
+    }
+}
+
+module.exports = {
+    Fighter,
+    OtherClass
+}
