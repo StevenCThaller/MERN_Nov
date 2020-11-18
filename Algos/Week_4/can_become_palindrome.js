a329ba26faf976b710d73d0fa97bd0bb3e60b761
Cody Thaller <stevencthaller@gmail.com>
2020-11-18 09:09:43 -0800

    Algo prompt 11/18

---
+++ Algos/Week_4/can_become_palindrome.js
@@ -0,0 +1,10 @@
+/* 
+    Given a string
+    return whether or not it's possible to make a palindrome out of the string's characters
+
+    What is it about a string that makes it possible for it to be a palindrome?
+*/
+
+function canBecomePalindrome(string){
+    
+}
