908b38d500e534d150fad482ef8acc915eed4423
Cody Thaller <stevencthaller@gmail.com>
2020-11-19 08:48:37 -0800

    Algo prompt 11/19

--- Algos/Week_4/can_become_palindrome.js
+++ Algos/Week_4/can_become_palindrome.js
@@ -6,5 +6,33 @@
 */
 
 function canBecomePalindrome(string){
-    
+    if (str.length === 0) { // if the string is empty it can't be a palindrome
+        return false;
+    }
+
+    const freqTable = {}; // this is going to keep track of how many times a character appears
+
+    for (const char of str) { // we want to go through the string and populate the frequency table
+        if(freqTable.hasOwnProperty(char)){ // if it's in the frequency table, increment the count
+            freqTable[char]++;
+        } else { // otherwise set it to one
+            freqTable[char] = 1;
+        }
+    }
+
+    // now, the trick to determining if the string can become a palindrome is that
+    // only 1 character can appear an odd number of times
+    let oddCount = 0; // keeping track of how many characters show up an odd number of times
+
+    for(const key in freqTable){ // loop through the frequency table
+        if(freqTable[key] % 2 == 1){ // and if the character count is odd, increment oddcount
+            oddCount++;
+        }
+
+        if(oddCount > 1){ // if oddCount is greater than one, it can't be a palindrome
+            return false;
+        }
+    }
+
+    return true; // if we made it out, it can be a palindrome!
 }
---
+++ Algos/Week_4/question_marks.js
@@ -0,0 +1,20 @@
+/* 
+    Given a string, containing letters, single digit ints, and question marks
+    return whether or not there are exactly 3 question marks between EVERY two ints that add up to 10
+        - if there are no two ints that add up to 10, return false
+    Helpful functions:
+        parseInt(char) => NaN or the string parsed to an int
+        isNaN(x) => true or false
+            - need to use isNaN if you want to know if something is NaN
+            - the number 0 is falsy
+            - NaN is falsy
+    
+    EXAMPLE:
+    questionMarks("acc?7??sss?3rr1??????5") will return true, because there are 3 question marks between 7 and 3
+
+    questionMarks("aa2?1??aalkg") will return false, because there are not 3 question marks between 2 and 1
+*/
+
+function questionMarks(string){
+    
+}
