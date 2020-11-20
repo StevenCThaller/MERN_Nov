8242ff14a6dd436240ee9acf56e5c1fd456346bf
Cody Thaller <stevencthaller@gmail.com>
2020-11-20 08:54:29 -0800

    Last algos :'(

---
+++ Algos/Week_4/honorific_versions.js
@@ -0,0 +1,26 @@
+/* 
+    Given an honorific (name title) and array of full name strings,
+    in "LastName, FirstName" format
+
+    Return a new array of strings in this format: "Honorific FirstName LastName"
+    Bonus: re-write it with built in functional programming methods
+*/
+function addHonorific(honorific, names){
+    
+}
+
+
+/* 
+    https://leetcode.com/problems/compare-version-numbers/
+    Given two strings, version1, and version2, both representing version numbers
+    If version1 > version2 return 1; if version1 < version2 return -1; otherwise return 0.
+    Helpful methods:
+        - .split(characterToSplitOn)
+            - returns an array of items: "a-b-c".split("-") returns ["a", "b", "c"]
+        - .parseInt
+            - given a string, converts it to and returns int or NaN (Not a Number) if conversion fails
+    Bonus, solve without .split
+*/
+function compareVersionNumbers(v1, v2){
+
+}
--- Algos/Week_4/question_marks.js
+++ Algos/Week_4/question_marks.js
@@ -13,8 +13,35 @@
     questionMarks("acc?7??sss?3rr1??????5") will return true, because there are 3 question marks between 7 and 3
 
     questionMarks("aa2?1??aalkg") will return false, because there are not 3 question marks between 2 and 1
+
+    questionMarks("aot4?ab?xyz6lkajhsglh???23??") will return false because 4 and 6 DO add up to 10, but there are not 3 question marks between them
 */
 
 function questionMarks(string){
-    
+    let prevInt = 0; // for keeping track of a previous number
+    let qMarkCnt = 0; // for keeping count of the number of question marks between any 2 numbers
+    let sumTenExists = false; // to know if there is, in fact, a sum of 10
+
+    for (let i = 0; i < string.length; i++) { // loop through the length of the string
+        if (string[i] === "?") { // if the character we're at is a question mark, add it to the counter
+            ++qMarkCnt;
+        } else { 
+            const parsed = parseInt(string[i]); // we'll check this character to see if it's a number or not
+
+            // not not a number means it's a number
+            if (!isNaN(parsed)) { // if the character is a number
+                if (prevInt + parsed === 10) { // check to see if the number plus the last number encountered equals 10
+                    sumTenExists = true; // if so, toggle sumTenExists to true
+
+                    if (qMarkCnt !== 3) { // BUT, if we haven't counted 3 question marks since that number, then return false
+                        return false;
+                    }
+                } else { // otherwise, reset everything.
+                    prevInt = parsed;
+                    qMarkCnt = 0;
+                }
+            }
+        }
+    }
+    return sumTenExists; // if we finish going through everything and sumTenExists is still true, return true, otherwise false
 }
